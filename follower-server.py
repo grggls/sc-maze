c3f80b5d2a13610758e9b2540004d81d229401b3
grggls <gregory.damiani@gmail.com>
2017-12-31 09:14:38 +0100

    keep the transport object for Users clients so we can write messages to them later

--- follower-server.py
+++ follower-server.py
@@ -4,21 +4,25 @@ from twisted.internet.protocol import Protocol, Factory, ClientFactory
 
 
 class EventProtocol(Protocol):
+    # events might come in chunks, handle responsibly
     def dataReceived(self, data):
         self.splitEvent(data)
 
     # split along delimiter (\n), chomp trailing newline as unused, grab event id 
-    # before storing away so we can put these in order. use ensure we have utf8
+    # before storing away so we can put these in order. ensure we have utf8
     def splitEvent(self, payload):
         for event in payload.split(delimiter)[:-1]:
             events[int(event[0])] = event.decode('utf-8')
 
 
 class UserProtocol(Protocol):
+    # only data received from user should be userId
     def dataReceived(self, data):
         userId = data.strip(delimiter)
-        users[userId] = 'foo' + userId
+        users[userId] = self.transport
 
+    def sendUserMessage(self, user, data):
+        users[user].sendMessage(data)
 
 if __name__ == '__main__':
     events = {}
