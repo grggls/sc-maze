dab1bf7cf327cf727ef5cdf101e7590cb37b2554
grggls <gregory.damiani@gmail.com>
2017-12-31 08:37:00 +0100

    cleanup some unnecessary python code. start storing the data we receive in global vars. somewhat tricky event parsing logic starting to get factored out into one place.

--- follower-server.py
+++ follower-server.py
@@ -1,27 +1,39 @@
-from twisted.internet.protocol import Protocol, Factory
 from twisted.internet import reactor
+from twisted.internet.endpoints import TCP4ClientEndpoint, connectProtocol
+from twisted.internet.protocol import Protocol, Factory, ClientFactory
 
-class EventSource(Protocol):
-    def dataReceived(self, data):
-        print 'EventSource data:' + data
-        #self.transport.write('EventSource data:' + data)
-        #self.transport.loseConnection()
 
-class UserClients(Protocol):
+class EventProtocol(Protocol):
     def dataReceived(self, data):
-        print 'UserClients data:' + data
-        #self.transport.write('UserClients data:' + data)
-        #self.transport.loseConnection()
+        self.splitEvent(data)
+
+    # split along delimiter (\n), chomp trailing newline as unused, grab event id 
+    # before storing away so we can put these in order. use ensure we have utf8
+    def splitEvent(self, payload):
+        for event in payload.split(delimiter)[:-1]:
+            events[int(event[0])] = event.decode('utf-8')
+
+
+class UserProtocol(Protocol):
+    def dataReceived(self, data):
+        userId = data.strip(delimiter)
+        users[userId] = 'foo' + userId
+
 
 if __name__ == '__main__':
+    events = {}
+    users = {}
+    followers = {}
+
+    delimiter = '\n'
+    separator = '|'
+
     event_source_factory = Factory()
-    event_source_factory.protocol = EventSource
+    event_source_factory.protocol = EventProtocol
 
     user_clients_factory = Factory()
-    user_clients_factory.protocol = UserClients
+    user_clients_factory.protocol = UserProtocol
 
     reactor.listenTCP(9090, event_source_factory)
-    print 'Event Source listener started'
     reactor.listenTCP(9099, user_clients_factory)
-    print 'User Clients  listener started'
     reactor.run()
