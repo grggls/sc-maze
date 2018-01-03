6de1a2ecced248b3fd76a5d4d0dd553538a60359
grggls <gregory.damiani@gmail.com>
2018-01-03 19:15:48 +0100

    aggressive logging. remove dispatch method.

--- follower-server.py
+++ follower-server.py
@@ -2,6 +2,9 @@ from twisted.internet import reactor, threads, protocol
 from twisted.internet.task import LoopingCall
 from twisted.internet.endpoints import TCP4ClientEndpoint, connectProtocol
 from twisted.internet.protocol import Protocol, Factory, ClientFactory
+from twisted.python import log
+import sys
+
 
 class EventFactory(protocol.ClientFactory):
     def __init__(self):
@@ -12,15 +15,18 @@ class EventFactory(protocol.ClientFactory):
         p.factory = self
         return p
 
+
 class EventProtocol(Protocol):
     def dataReceived(self, data):
         # split along delimiter (\n), chomp trailing newline as unused, grab event id
         # before storing away so we can put these in order. ensure we have utf8
         message = data.rstrip()
         for event in message.split(delimiter):
+            log.msg("%s received" % event)
             sequence = event.split(separator)[0]
             try:
                 events[int(sequence)] = event
+                log.msg("events: %s" % events)
             except:
                 pass    # bad event?
 
@@ -37,40 +43,39 @@ class UserProtocol(Protocol):
         users[int(userId)] = self
         followers[int(userId)] = []
 
-def messageDispatcher(message):
-    fields = message.split(separator)
-    sequence = fields[0]
-    msgType = fields[1]
-    fromId = int(fields[2]) if len(fields) > 2 else None
-    toId = int(fields[3]) if len(fields) > 3 else None
 
-    if msgType == 'F':
-        followers[toId].append(fromId)
-        users[toId].transport.write(message)
-    elif msgType == 'U':
-        if fromId in followers[toId]:
-            followers[toId].remove(fromId)
-    elif msgType == 'B':
-        for user in users:
-            user.transport.write(message)
-    elif msgType == 'P':
-        users[toId].transport.write(message)
-    elif msgType == 'S':
-        if followers[fromId]:
-            for follower in followers[fromId]:
-                users[follower].transport.write(message)
-    else:
-        print "ERROR: unhandled message type on message %s" % message
-        pass
+def eventDispatcher(events, nextEvent):
+    if nextEvent in events:
+        fields = events[nextEvent].split(separator)
+        sequence = int(fields[0])
+        msgType = fields[1]
+        fromId = int(fields[2]) if len(fields) > 2 else None
+        toId = int(fields[3]) if len(fields) > 3 else None
 
-def blockingEventDispatch(events, nextEvent):
-    try:
-        if nextEvent in events:
-            messageDispatcher(events[nextEvent])
-            nextEvent = nextEvent + 1
-    except KeyError:
-        print "Error for %s : %s processing" % nextEvent, events[nextEvent]
-        pass   # should never get here with the if above
+        log.msg("processing: %s" % events[nextEvent])
+        nextEvent += 1
+
+        if msgType == 'F':
+            followers[toId].append(fromId)
+            users[toId].transport.write(message)
+            log.msg("%s to follow %s" % fromId, toId)
+        elif msgType == 'U':
+            if fromId in followers[toId]:
+                followers[toId].remove(fromId)
+                log.msg("%s to unfollow %s" % toId, fromId)
+        elif msgType == 'B':
+            for user in users:
+                user.transport.write(message)
+                log.msg("broadcasting %s to %s" % message, user)
+        elif msgType == 'P':
+            users[toId].transport.write(message)
+            log.msg("%s sending private message to %s" % fromId, toId)
+        elif msgType == 'S':
+            if followers[fromId]:
+                for follower in followers[fromId]:
+                    users[follower].transport.write(message)
+                    log.msg("sending status %s to user %s" % message, users[follower])
+
 
 if __name__ == '__main__':
     events = {}     # store all events here, keep full string, key is sequence #
@@ -89,11 +94,14 @@ if __name__ == '__main__':
     userClientsFactory.protocol = UserProtocol
     userClientsFactory.clients = []
 
+    log.startLogging(sys.stdout)
+
     try:
-        LoopingCall(blockingEventDispatch, events, nextEvent).start(0.1)
+        LoopingCall(eventDispatcher, events, nextEvent).start(0.0001)
     except Exception as err:
         print "ERROR starting looping call: %s" % err
 
     reactor.listenTCP(9090, eventSourceFactory)
     reactor.listenTCP(9099, userClientsFactory)
     reactor.run()
+
